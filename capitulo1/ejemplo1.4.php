4e901818f55157dc9c3024c9d192fc1054fb25a8
Juan Torres <juanin.torres@gmail.com>
2013-04-23 02:00:41 -0300

    Algo mas

--- capitulo1/ejemplo1.4.php
+++ capitulo1/ejemplo1.4.php
@@ -1,12 +1,29 @@
 <?php
 
-$i = 1;
+// Esto es un comentario
 
+/*
+$i = 1;
 while($i < 5)
 {
     echo "<h{$i}>Esto es un h{$i}</h{$i}>";
     $i++;
 }
+*/
+
+if($_GET['v']< 0)
+{
+    echo 'v es negativo';
+}
+elseif($_GET['v']>10)
+{
+    echo 'v es positivo y mayor que 10';
+}
+else
+{
+    echo 'v es positivo';
+}
+
 
 
 ?>
