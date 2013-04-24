80414e246ab6d4c4bd8caea5abb987efac94c752
Juan Torres <juanin.torres@gmail.com>
2013-04-23 23:54:33 -0300

    Agregando una sumatoria

---
+++ capitulo1/ejemplo1.6.php
@@ -0,0 +1,23 @@
+<?php
+
+$n = $_GET['n'];
+
+if(is_numeric($n) && ($n > 1) )
+{
+    // Me pasaron un numero
+    $i = 1;
+    $sumatoria = 0;
+    while($i <= $n)
+    {
+        $sumatoria = $sumatoria + $i;
+        $i++;
+    }
+    echo "La sumatoria desde 1 hasta {$n} es: {$sumatoria}";
+}
+else
+{
+    echo "<span style='font-size: 24px; color: red;'>ESTUPIDO, TE DIJE UN NUMERO !!!!</span>";
+}
+
+
+?>
