a22e1e61185b5a0eb5b8f4a595952d3768c58ee2
phillip mendoza gonzales <phillipmg.15@gmail.com>
2013-04-30 21:11:10 -0400

    tarea
    probando tarea

---
+++ capitulo2/ejemplo2.3.php
@@ -0,0 +1,76 @@
+<?php
+
+function getHeader()
+{
+    $h = 'Esto es el header';
+    return $h;
+}
+
+function getMenu($actual='home')
+{
+    $elementosMenu = array(
+        'inicio' => array(
+            'url'   => 'inicio.html',
+            'texto' => 'inicio'
+            ),
+        'noticias' => array(
+            'url'   => 'noticias.html',
+            'texto' => 'noticias'
+            ),
+        'cancionesnuevas' => array(
+            'url'   => 'cancionesnuevas.html',
+            'texto' => 'cancionesnuevas'
+            )
+        'videos' => array(
+            'url'   => 'videos.html',
+            'texto' => 'videos'
+            )
+        'letras' => array(
+            'url'   => 'letras.html',
+            'texto' => 'letras'
+            )
+        'eventos' => array(
+            'url'   => 'eventos.html',
+            'texto' => 'eventos'
+            )
+        'contacto' => array(
+            'url'   => 'contacto.html',
+            'texto' => 'Contacto'
+            )
+        );
+
+    $menu = "<ul>";
+    foreach ($elementosMenu as $index => $seccion)
+    {
+        $current = ($index==$actual)?'current':'';
+        $menu .= "<li class='{$current}'><a href='{$seccion['url']}'>{$seccion['texto']}</a></li>";
+    }
+    $menu .= "</ul>";
+    return $menu;
+}
+
+function getFooter()
+{
+    return 'Esto es FOOTER !!!';
+}
+
+
+?>
+
+<style>
+    li{
+        background: #EB1D22;
+        display: inline-block;
+        height: 1.1em;
+        width: 1000px;
+        max-width: 100%;
+        margin: 0 auto;
+
+    }
+    li.current{
+        background:white;
+    }
+</style>
+
+
+<div><?php echo getMenu($_GET['v']); ?></div>
